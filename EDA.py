64dbbb143d726361fd43edbf3e8ce499d1c91dcd
athena15 <>
2020-05-19 20:15:07 +0000

    First commit
    Add EDA.py.

---
+++ EDA.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %sh pip install koalas
+
+# COMMAND ----------
+
+import pandas as pd
+import numpy as np
+import databricks.koalas as ks
+
+# COMMAND ----------
+
