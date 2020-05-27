e6c44762aa6becd79b53572e723f687b9de5b623
Brenner Heintz <brenner.heintz@gmail.com>
2020-05-27 16:02:50 -0700

    Add commit #2

--- 1 - EDA.py
+++ 1 - EDA.py
@@ -15,24 +15,4 @@ print(x)
 # COMMAND ----------
 
 y = 10
-z = 20
-print(y+z)
 
-# COMMAND ----------
-
-a = 2
-b = 3
-c = 5
-d = 10
-e = 20
-
-# COMMAND ----------
-
-print('This is a change!')
-print('This is another change!')
-print("And finally, a third change!")
-
-# COMMAND ----------
-
-def squared(x):
-  return x **2
